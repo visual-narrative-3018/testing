717853bad6b6e447f78dcce2f06b327ddb6e0fb2
Anastasia Egorova <aegorov@ncsu.edu>
2018-12-08 13:34:32 -0500

    Add test puzzle

---
+++ Teleport/Assets/Scripts/SwitchPlaces.cs
@@ -0,0 +1,133 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+
+public class SwitchPlaces : VRTK.VRTK_InteractableObject
+{
+    public int speed = 10;
+    public int totalSwaps = 13;
+    public GameObject[] originalArray = new GameObject[6];
+    public GameObject obj;
+    private readonly Vector3[] startingPositions = new Vector3[6];
+    private GameObject[] orderedArray;
+    private int total;
+    private int chosenIndex = -1;
+    private Boolean doneMoving = true;
+    private Color color = Color.white;
+
+    void Start()
+    {
+        orderedArray = (GameObject[]) originalArray.Clone();
+        total = orderedArray.Length;
+        UpdateStartingPositions();
+        color.a = 0.66f;
+        StartCoroutine(RandomizeOrder());
+    }
+
+
+    public override void StartUsing(VRTK.VRTK_InteractUse currentUsingObject = null)
+    {
+        base.StartUsing(currentUsingObject);
+        if (doneMoving)
+        {
+            if (Array.IndexOf(orderedArray, obj) > -1)
+            {
+                int newIndex = Array.IndexOf(orderedArray, obj);
+                if (chosenIndex != -1)
+                {
+                    orderedArray[chosenIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+                }
+                if (newIndex != chosenIndex)
+                {
+                    orderedArray[newIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().color = color;
+                    chosenIndex = newIndex;
+                }
+                else
+                {
+                    chosenIndex = -1;
+                }
+            }
+        }
+    }
+
+    public override void StopUsing(VRTK.VRTK_InteractUse previousUsingObject = null, bool resetUsingObjectState = true)
+    {
+        base.StopUsing(previousUsingObject, resetUsingObjectState);
+    }
+
+
+    void UpdateStartingPositions()
+    {
+        for (int i = 0; i < total; i++)
+        {
+            startingPositions[i] = orderedArray[i].transform.position;
+        }
+    }
+
+    IEnumerator RandomizeOrder()
+    {
+        for (int i = 0; i < totalSwaps; i++)
+        {
+            while (!doneMoving) {
+                yield return null;
+            }
+            StartCoroutine(SwapPositions(UnityEngine.Random.Range(0, total), UnityEngine.Random.Range(0, total), true));
+        }
+    }
+
+    IEnumerator SwapPositions(int a, int b, Boolean isStarting = false)
+    {
+        //To prevent the swap before another one is finished
+        doneMoving = false;
+        while (Vector3.Distance(orderedArray[a].transform.position, startingPositions[b]) > 0.01f ||
+               Vector3.Distance(orderedArray[b].transform.position, startingPositions[a]) > 0.01f)
+        {
+            orderedArray[a].transform.position = Vector3.MoveTowards(orderedArray[a].transform.position, startingPositions[b], speed * Time.deltaTime);
+            orderedArray[b].transform.position = Vector3.MoveTowards(orderedArray[b].transform.position, startingPositions[a], speed * Time.deltaTime);
+            yield return null;
+        }
+
+        orderedArray[a].transform.position = startingPositions[b];
+        orderedArray[b].transform.position = startingPositions[a];
+
+        GameObject temp = orderedArray[a];
+        orderedArray[a] = orderedArray[b];
+        orderedArray[b] = temp;
+
+        UpdateStartingPositions();
+        doneMoving = isStarting || CheckComplete();
+    }
+
+    Boolean CheckComplete()
+    {
+        for (int i = 0; i < total; i++)
+        {
+            if (!originalArray[i].name.Equals(orderedArray[i].name))
+            {
+                return true;
+            }
+        }
+        orderedArray[chosenIndex].transform.GetChild(0).GetComponent<SpriteRenderer>().color = Color.white;
+        chosenIndex = -1;
+        return false;
+    }
+
+    protected override void Update()
+    {
+        base.Update();
+        //OnClick
+
+        if (doneMoving && chosenIndex >= 0)
+        {
+            if (OVRInput.GetUp(OVRInput.Button.SecondaryThumbstick) && chosenIndex > 0)
+            {
+                StartCoroutine(SwapPositions(chosenIndex--, chosenIndex));
+            }
+
+            if (OVRInput.GetUp(OVRInput.Button.PrimaryThumbstick) && chosenIndex < total - 1)
+            {
+                StartCoroutine(SwapPositions(chosenIndex++, chosenIndex));
+            }
+        }
+    }
+}
---
+++ Teleport/Assets/Scripts/Teleporter.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Teleporter : MonoBehaviour {
+
+    public GameObject TeleportMarker;
+    public Transform Player;
+    public float RayLength = 50f;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log("Started");
+        transform.position = Player.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, transform.forward, out hit, RayLength)) {
+            // Debug.DrawRay(transform.position, transform.forward * hit.distance, Color.blue, 10);
+            if (hit.collider.tag == "Ground") {
+                if (!TeleportMarker.activeSelf) {
+                    TeleportMarker.SetActive(true);
+                }
+                TeleportMarker.transform.position = hit.point;
+            } else {
+                TeleportMarker.SetActive(false);
+            }
+        } else {
+            // Debug.DrawRay(transform.position, transform.forward * 1000, Color.red, 10);
+            TeleportMarker.SetActive(false);
+        }
+        if (Input.GetMouseButtonDown(0)) //OVRInput.GetDown(OVRInput.Button.One)
+        {
+            Debug.Log("Click");
+            // check where the user is looking to teleport
+            if (TeleportMarker.activeSelf)
+            {
+                Vector3 markerPosition = TeleportMarker.transform.position;
+                Player.position = new Vector3(markerPosition.x, Player.position.y, markerPosition.z);
+            }
+        }
+	}
+}
